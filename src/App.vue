a3e099b85d1891c59700b2b53d519089c76b2a96
gregatgit <gregdd@outlook.com>
2020-01-06 09:39:26 +1000

    pass todos to list

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div id="app">
-    <Header></Header>
+    <Header />
+    <todo-list :todos="todos" />
   </div>
 </template>
 
 <script>
-
 import Header from './components/Header'
+import TodoList from './components/TodoList'
 
 export default {
   name: 'app',
+  data: function() {
+    return {
+      todos: [
+        { name: 'walk dog', done: false },
+        { name: 'build app', done: false },
+      ],
+    }
+  },
   components: {
-    Header
-  }
+    Header,
+    TodoList
+  },
 }
 </script>
 
---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>List To Be Done</h2>
+    <ul>
+      <li v-for="(todo, index) in todos">{{todo.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'todo-list',
+    props: ['todos']
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
